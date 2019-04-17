19fed64a334cf024174d6d2fc33904631a5af592
CarlosIsma <carlos.lucero@epn.edu.ec>
2019-04-16 19:46:22 -0500

    ejercicio2

--- ejercicio2/index.php
+++ ejercicio2/index.php
@@ -4,4 +4,16 @@
  *
  * Implementar una función que muestre un título centrado en pantalla, y la llamaremos posteriormente dos veces.
 */
+
+function mostrartitulo($txt)
+{
+  echo "<h1 style=\"text-align:center\">";
+  echo $txt;
+  echo "</h1>";
+}
+
+mostrartitulo("Primer titulo");
+echo "<br>";
+mostrartitulo("Segundo Titulo");
+
 ?>
